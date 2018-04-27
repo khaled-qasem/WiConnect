d99b1ed93b2ccafc58204d8a2f6df67ae98bf13f
Khaled Qasem <khaled.alqerem@gmail.com>
2018-04-27 17:22:48 +0300

    Initial commit

---
+++ app/src/main/java/wiconnect/khaled/com/wiconnect/utils/Logger.java
@@ -0,0 +1,50 @@
+package wiconnect.khaled.com.wiconnect.utils;
+
+import android.util.Log;
+
+import wiconnect.khaled.com.wiconnect.BuildConfig;
+
+/**
+ * Created by Khaled on 4/2 7/2018.
+ * Assumptions
+ * Descriptions
+ */
+
+public class Logger {
+
+    private String LOG_TAG;
+
+    private Logger(Class clazz) {
+        this.LOG_TAG = clazz.getSimpleName();
+    }
+
+    public static Logger loggerFor(Class clazz) {
+        return new Logger(clazz);
+    }
+
+    public void d(String message) {
+        if (BuildConfig.DEBUG) {
+            Log.d(LOG_TAG, message);
+        }
+    }
+
+    public void d(String message, Throwable throwable) {
+        if (BuildConfig.DEBUG) {
+            Log.d(LOG_TAG, message, throwable);
+        }
+    }
+
+    public void i(String message) {
+        if (BuildConfig.DEBUG) {
+            Log.i(LOG_TAG, message);
+        }
+    }
+
+    public void e(String message) {
+        Log.e(LOG_TAG, message);
+    }
+
+    public void e(String message, Throwable throwable) {
+        Log.e(LOG_TAG, message, throwable);
+    }
+}
